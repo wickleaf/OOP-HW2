1480916eefc92b32fe57398efb4265db8557c6bd
wickleaf <rehmansaqib55@gmail.com>
2025-10-18 15:04:40 +0500

    i want to kill myself

--- BigNum.cpp
+++ BigNum.cpp
@@ -10,8 +10,8 @@ BigNum::BigNum() {
     digits.push_back(0); 
 }
 BigNum::BigNum(const BigNum& bigNum) {
-    isNegative = bigNum.isNegative;
-    digits = bigNum.digits; 
+    this -> isNegative = bigNum.isNegative;
+    this -> digits = bigNum.digits; 
 }
 BigNum::BigNum(const std::string& bigStr){
     int x = bigStr.length();
@@ -49,3 +49,102 @@ void BigNum::clear() {
 BigNum::~BigNum() {
     clear();
 }
+
+void BigNum::input() {
+    clear();
+    string inputStr;
+    while (true) {
+        cin >> inputStr;
+        if(cin.fail()) {
+            cout << "Wrong fucking datatype retard." << endl;
+            cin.clear();
+    }
+        else break;
+    }
+    
+
+    if (inputStr[0] == '-') {
+        isNegative = true;
+    } else {
+        isNegative = false;
+    }
+    for (char ch : inputStr) {
+        if (isdigit(ch)) {
+            digits.push_back(ch); 
+        }
+        else{
+            cout << "Invalid number"<<endl;
+            return;
+        }
+    }
+    if (digits.empty()) {
+        digits.push_back(0); 
+    }
+}
+
+void BigNum::print(){
+    int count = 0;
+    string outputStr ="";
+    for(int i=digits.size(); i<0; i++){
+        if (count%3==0){
+            outputStr= ","+outputStr;
+            outputStr= digits[i]+outputStr;
+            count++;
+        }
+        else{
+            outputStr= digits[i]+outputStr;
+        }
+    }
+}
+
+void BigNum::inputFromFile(const string& fileName){
+    ifstream inputFile(fileName);
+    string line;
+    getline(inputFile, line);
+
+    if (line[0] == '-') {
+        isNegative = true;
+    } else {
+        isNegative = false;
+    }
+    for (char ch : line) {
+        if (isdigit(ch)) {
+            digits.push_back(ch); 
+        }
+        else{
+            cout << "Invalid number"<<endl;
+            return;
+        }
+    }
+    if (digits.empty()) {
+        digits.push_back(0); 
+    }
+
+}
+void BigNum::printToFile(const string& fileName){
+    ofstream outputFile(fileName);
+    if (isNegative) {
+        outputFile << "-";
+    }
+    for (int i = digits.size() - 1; i >= 0; i--) {
+        outputFile << static_cast<char>(digits[i]);
+    }
+    outputFile.close();
+}
+
+void BigNum::copy(const BigNum& bigNum){
+    this -> isNegative = bigNum.isNegative;
+    this -> digits = bigNum.digits; 
+}
+void BigNum::operator=(const BigNum& bigNum){
+    this -> isNegative = bigNum.isNegative;
+    this -> digits = bigNum.digits; 
+}
+void BigNum::zerofy(){
+    isNegative = false;
+    digits.clear();
+    digits.push_back(0); 
+}   
+
+BigNum BigNum::add(const BigNum& bigNum){
+    BigNum result;
